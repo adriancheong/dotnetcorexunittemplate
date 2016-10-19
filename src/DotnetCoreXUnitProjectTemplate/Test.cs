0689b30a8eccfb283cc9ae1b62e6f796bf61a467
Adrian Cheong <adriancheong@gic.com.sg>
2016-10-19 18:31:41 +0800

    Initial Add

---
+++ src/DotnetCoreXUnitProjectTemplate/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotnetCoreXUnitProjectTemplate
+{
+    /**************************************************
+     * 
+     * To use:
+     * Either use cmd or powershell and cd to the project directory where project.json is
+     *      dotnet restore && dotnet build && dotnet test
+     * OR
+     * Use Visual Studio GUI Test Runner (Ctrl R,T)
+     * ************************************************/
+    public class Test
+    {
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
